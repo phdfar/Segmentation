180b401913eb488c605075b34e024772f50b1c93
phdfar <103323098+phdfar@users.noreply.github.com>
2022-04-29 19:14:19 +0430

    Update accuracy.py

--- Image_segmentation/accuracy.py
+++ Image_segmentation/accuracy.py
@@ -1,6 +1,8 @@
 from tensorflow.keras.preprocessing.image import load_img
 import pandas as pd
-def run(test_preds,allpath,name):
+import data
+
+def run(test_preds,allpath,name,args):
   Taccuracy=0
   Tprecision=0
   Trecall=0
@@ -8,7 +10,18 @@ def run(test_preds,allpath,name):
   full_result=[]
   for ii in range(len(test_preds)):
 
-    gt = load_img(test_target_img_paths[ii],target_size=img_size, color_mode="grayscale")
+    path = allpath[ii]
+    frameindex= list(path.keys())[0]
+    #imagepath = path[frameindex][0]
+    #tep.append(imagepath)
+    seq = path[frameindex][1]
+    mask = seq.load_one_masks([frameindex])
+    # resize image
+    dim = (args.imagesize[1],args.imagesize[0])
+    gtn = cv2.resize(mask, dim, interpolation = cv2.INTER_NEAREST)
+            
+    """
+    gtn = load_img(test_target_img_paths[ii],target_size=img_size, color_mode="grayscale")
     gtn = np.array(gt)
     ys = gtn.copy()
     ys[np.where(gtn==2)]=0
@@ -20,7 +33,7 @@ def run(test_preds,allpath,name):
     fg = frame[:,:,1]
     fg[fg<0.6]=0;
     #new_frame=np.concatenate((bg.reshape(320,320,1),fg.reshape(320,320,1)),axis=2)
-
+    """
     mask = np.argmax(frame, axis=-1)
     mask = np.expand_dims(mask, axis=-1)
     mask = mask[:,:,0];
