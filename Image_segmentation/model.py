c8492eb5acc314b4976315c74d6ba3da566a8ac1
phdfar <103323098+phdfar@users.noreply.github.com>
2022-04-30 19:29:22 +0430

    Update model.py

--- Image_segmentation/model.py
+++ Image_segmentation/model.py
@@ -1,11 +1,13 @@
 from tensorflow.keras import layers
 from tensorflow import keras
-
+from deformable_conv_layer import DeformableConvLayer
 
 def network(args):
   if args.network=='inception_default':
     return inception_default(args.imagesize,2)
-
+  if args.network=='inception_deform':
+    return inception_deform(args.imagesize,2)
+  
 def inception_default(img_size, num_classes):
     inputs = keras.Input(shape=img_size + (3,))
 
@@ -63,3 +65,62 @@ def inception_default(img_size, num_classes):
     model = keras.Model(inputs, outputs)
     return model
 
+  def inception_deform(img_size, num_classes):
+    inputs = keras.Input(shape=img_size + (3,))
+
+    ### [First half of the network: downsampling inputs] ###
+
+    # Entry block
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    # Blocks 1, 2, 3 are identical apart from the feature depth.
+    for filters in [64, 128, 256]:
+        x = layers.Activation("relu")(x)
+        #x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = DeformableConvLayer(filters, [3, 3], num_deformable_group=1,padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        #x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = DeformableConvLayer(filters, [3, 3], num_deformable_group=1,padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        #residual = layers.Conv2D(filters, 1, strides=2, padding="same")(previous_block_activation)
+        residual = DeformableConvLayer(filters, [1, 1], strides=2, num_deformable_group=1,padding="same")(previous_block_activation)
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    ### [Second half of the network: upsampling inputs] ###
+
+    for filters in [256, 128, 64, 32]:
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.UpSampling2D(2)(x)
+
+        # Project residual
+        residual = layers.UpSampling2D(2)(previous_block_activation)
+        #residual = layers.Conv2D(filters, 1, padding="same")(residual)
+        residual = DeformableConvLayer(filters, [1, 1], num_deformable_group=1,padding="same")(residual)
+
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    # Add a per-pixel classification layer
+    #outputs = layers.Conv2D(num_classes, 3, activation="softmax", padding="same")(x)
+    outputs = DeformableConvLayer(num_classes, [3, 3], num_deformable_group=1,padding="same")(x)
+    # Define the model
+    model = keras.Model(inputs, outputs)
+    return model
