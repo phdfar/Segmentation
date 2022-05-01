4311bc92acd0a1a0a5d3f9390a71b94887d2383d
phdfar <iplserver01@gmail.com>
2022-05-01 14:33:38 +0000

    add img

--- Image_segmentation/model.py
+++ Image_segmentation/model.py
@@ -5,8 +5,8 @@ from deformable_conv_layer import DeformableConvLayer
 def network(args):
   if args.network=='inception_default':
     return inception_default(args.imagesize,2)
-  if args.network=='inception_deform':
-    return inception_deform(args.imagesize,2)
+  if args.network=='inception_mobilenet':
+    return inception_mobilenet(args.imagesize,2)
   
 def inception_default(img_size, num_classes):
     inputs = keras.Input(shape=img_size + (3,))
@@ -65,14 +65,12 @@ def inception_default(img_size, num_classes):
     model = keras.Model(inputs, outputs)
     return model
 
-def inception_deform(img_size, num_classes):
+def inception_mobilenet(img_size, num_classes):
     inputs = keras.Input(shape=img_size + (3,))
-
     ### [First half of the network: downsampling inputs] ###
 
     # Entry block
-    #x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
-    x = DeformableConvLayer(32, [3, 3],strides=2, num_deformable_group=1,padding="same")(inputs)
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
     x = layers.BatchNormalization()(x)
     x = layers.Activation("relu")(x)
 
@@ -81,24 +79,34 @@ def inception_deform(img_size, num_classes):
     # Blocks 1, 2, 3 are identical apart from the feature depth.
     for filters in [64, 128, 256]:
         x = layers.Activation("relu")(x)
-        #x = layers.SeparableConv2D(filters, 3, padding="same")(x)
-        x = DeformableConvLayer(filters, [3, 3], num_deformable_group=1,padding="same")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
         x = layers.BatchNormalization()(x)
 
         x = layers.Activation("relu")(x)
-        #x = layers.SeparableConv2D(filters, 3, padding="same")(x)
-        x = DeformableConvLayer(filters, [3, 3], num_deformable_group=1,padding="same")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
         x = layers.BatchNormalization()(x)
 
         x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
 
         # Project residual
-        #residual = layers.Conv2D(filters, 1, strides=2, padding="same")(previous_block_activation)
-        residual = DeformableConvLayer(filters, [1, 1], strides=2, num_deformable_group=1,padding="same")(previous_block_activation)
+        residual = layers.Conv2D(filters, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
         x = layers.add([x, residual])  # Add back residual
         previous_block_activation = x  # Set aside next residual
 
     ### [Second half of the network: upsampling inputs] ###
+
+    backbone = keras.applications.MobileNetV2(
+        weights="imagenet", include_top=False, input_shape=img_size + (3,)
+    )
+    backbone.trainable = False
+    z = keras.applications.mobilenet_v2.preprocess_input(inputs)
+    z = backbone(z)
+    z = layers.SeparableConv2D(480, 3, strides=1, padding="valid")(z)
+    z = layers.Reshape((12,20,64))(z)
+
+    x = layers.Concatenate(axis=-1)([x, z])
 
     for filters in [256, 128, 64, 32]:
         x = layers.Activation("relu")(x)
@@ -113,15 +121,13 @@ def inception_deform(img_size, num_classes):
 
         # Project residual
         residual = layers.UpSampling2D(2)(previous_block_activation)
-        #residual = layers.Conv2D(filters, 1, padding="same")(residual)
-        residual = DeformableConvLayer(filters, [1, 1], num_deformable_group=1,padding="same")(residual)
-
+        residual = layers.Conv2D(filters, 1, padding="same")(residual)
         x = layers.add([x, residual])  # Add back residual
         previous_block_activation = x  # Set aside next residual
 
     # Add a per-pixel classification layer
-    #outputs = layers.Conv2D(num_classes, 3, activation="softmax", padding="same")(x)
-    outputs = DeformableConvLayer(num_classes, [3, 3], num_deformable_group=1,padding="same")(x)
+    outputs = layers.Conv2D(2, 3, activation="softmax", padding="same")(x)
+
     # Define the model
     model = keras.Model(inputs, outputs)
     return model
