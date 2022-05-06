49749d67ccaf9dc0e48b6b4df2e4474cfb78f8bb
phdfar <103323098+phdfar@users.noreply.github.com>
2022-05-06 12:02:29 +0430

    Update path.py

--- Image_segmentation/path.py
+++ Image_segmentation/path.py
@@ -21,21 +21,32 @@ def getinfo(args):
     flag_multi=1;
     
   for i in meta_plus:
+    flag=0;
     if  args.num_instance==1000 and args.unq_class==1000:
       for c in i['unique_class']:
-        if c in list(args.classid):
-          valid.append(i['id'])
+        if c not in list(args.classid):
+          flag=1;
+      if flag==0:
+        valid.append(i['id'])
+        #print(i)
           
     elif args.num_instance==1000 and args.unq_class!=1000:
       if i['number_unique_class']==args.unq_class:
         for c in i['unique_class']:
-          if c in list(args.classid):
-            valid.append(i['id'])
+          if c not in list(args.classid):
+            flag=1;  
+        if flag==0:
+          valid.append(i['id'])
+          #print(i)
           
     elif i['number_instances']==args.num_instance and i['number_unique_class']==args.unq_class:
       for c in i['unique_class']:
-        if c in list(args.classid):
+        if c not in list(args.classid):
+          flag=1;  
+        if flag==0:
           valid.append(i['id'])
+          #print(i)
+
           #lenf+=i['clip_length']
           #print(i)
           
