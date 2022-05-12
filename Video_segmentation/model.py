78c2dfad893d1409f99e8501b8d3ec1519dfd4e8
phdfar <103323098+phdfar@users.noreply.github.com>
2022-05-12 11:47:10 +0430

    Update model.py

--- Video_segmentation/model.py
+++ Video_segmentation/model.py
@@ -5,14 +5,14 @@ import tensorflow as tf
 
 def network(args):
   if args.network=='inception_3d_default':
-    return inception_3d_default(args.imagesize,2)
+    return inception_3d_default(args.imagesize,2,args.subseq_length)
   if args.network=='inception_mobilenet':
     return inception_mobilenet(args.imagesize,2)
   if args.network=='mobilenet_s1':
     return mobilenet_s1(args.imagesize,2)
   
-def inception_3d_default(img_size, num_classes):
-  inputs = keras.Input(shape=img_size + (3*4,1))
+def inception_3d_default(img_size, num_classes,subseq_length):
+  inputs = keras.Input(shape=img_size + (3*subseq_length,1))
 
   x = tf.keras.layers.Conv3D(32, 3,strides=(2, 2, 1),padding='same')(inputs)
   x = layers.BatchNormalization()(x)
@@ -62,9 +62,13 @@ def inception_3d_default(img_size, num_classes):
   # Add a per-pixel classification layer
   y = layers.Conv3D(num_classes, 3, activation="softmax", padding="same")(x)
   outputs = layers.Reshape((y.shape[1],y.shape[2],y.shape[3]*y.shape[4]))(y)
-  o1,o2,o3,o4 = tf.split(outputs, num_or_size_splits=4, axis=-1)
+  if subseq_length==6:
+    o1,o2,o3,o4,o5,o6 = tf.split(outputs, num_or_size_splits=subseq_length, axis=-1)
+    model = keras.Model(inputs,[o1,o2,o3,o4,o5,o6])
+  elif subseq_length==4:
+    o1,o2,o3,o4 = tf.split(outputs, num_or_size_splits=subseq_length, axis=-1)
+    model = keras.Model(inputs,[o1,o2,o3,o4])
 
-  model = keras.Model(inputs,[o1,o2,o3,o4])
   return model
 
 def inception_default(img_size, num_classes):
