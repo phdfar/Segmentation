8f116e60aeab225aa41dcd61a2142b206962f030
phdfar <103323098+phdfar@users.noreply.github.com>
2022-05-14 12:42:53 +0430

    Update accuracy.py

--- Image_segmentation/accuracy.py
+++ Image_segmentation/accuracy.py
@@ -155,100 +155,44 @@ def run(test_preds,allpath,name,args,full_result):
 
   
   
-def run2(test_preds,allpath,name,args):
+def run_semantic(test_preds,allpath,name,args,y_pred,y_true):
+ flag_multi=0;
+  if  args.num_instance>1:
+        flag_multi=1;
+    
   Taccuracy=0
   Tprecision=0
   Trecall=0
   TFS=0
-  full_result=[]
   for ii in range(len(test_preds)):
-
     path = allpath[ii]
     frameindex= list(path.keys())[0]
     imagepath = path[frameindex][0]
     #tep.append(imagepath)
     seq = path[frameindex][1]
-    mask = seq.load_one_masks([frameindex])
+    flagmulti = path[frameindex][2]
+    if flagmulti==0:
+      mask = seq.load_one_masks([frameindex])
+    else:
+      mask = seq.load_multi_masks([frameindex]);
+            
     # resize image
     dim = (args.imagesize[1],args.imagesize[0])
     gtn = cv2.resize(mask, dim, interpolation = cv2.INTER_NEAREST)
             
-    """
-    gtn = load_img(test_target_img_paths[ii],target_size=img_size, color_mode="grayscale")
-    gtn = np.array(gt)
-    ys = gtn.copy()
-    ys[np.where(gtn==2)]=0
-    ys[np.where(gtn!=2)]=1
-    gtn = ys.copy()
 
-    frame=test_preds[ii]
-    bg = frame[:,:,0]
-    fg = frame[:,:,1]
-    fg[fg<0.6]=0;
-    #new_frame=np.concatenate((bg.reshape(320,320,1),fg.reshape(320,320,1)),axis=2)
-    """
     frame=test_preds[ii]
 
     mask = np.argmax(frame, axis=-1)
     mask = np.expand_dims(mask, axis=-1)
     mask = mask[:,:,0];
 
-    result = np.zeros((args.imagesize[0],args.imagesize[1],3),'uint8')
-    TP=0;FP=0;FN=0;TN=0;
-    for i in range(mask.shape[0]):
-      for j in range(mask.shape[1]):
-        if mask[i][j] == gtn[i][j] and mask[i][j]==1:
-          result[i][j][0]=0;result[i][j][1]=255;result[i][j][2]=0;
-          TP+=1;
-        elif mask[i][j] != gtn[i][j] and mask[i][j]==1:
-          result[i][j][0]=255;result[i][j][1]=0;result[i][j][2]=0;
-          FP+=1;
-        elif mask[i][j] != gtn[i][j] and mask[i][j]==0:
-          result[i][j][0]=255;result[i][j][1]=255;result[i][j][2]=0;
-          FN+=1;
-        else:
-          TN+=1;
+    
+    y_pred= y_pred  + mask.ravel().tolist()
+    y_true =y_true + gtn.ravel().tolist()
+    
+    print(metrics.confusion_matrix(y_true, y_pred))
+    print(metrics.classification_report(y_true, y_pred, digits=args.num_class))
 
-    accuracy = (TP + TN) / (TP + TN + FN + FP)
-    try:
-      precision = TP / (TP + FP)
-    except:
-      precision = 0
-    try:
-      recall = TP / (TP + FN)
-    except:
-      recall = 0
 
-    try:
-      FS = (2*recall*precision)/(precision+recall)
-    except:
-      FS=0
-
-    Taccuracy+=accuracy
-    Tprecision+=precision
-    Trecall+=recall
-    TFS+=FS
-
-    res = keras.preprocessing.image.array_to_img(result)
-    filename = imagepath.split('/'); filename=filename[-2]+'_'+filename[-1]
-    full_result.append((filename,accuracy,precision,recall,FS))
-    try:
-      os.mkdir('result')
-    except:
-      pass
-    res.save('result/'+filename)
-  lendata=len(test_preds)
-  tac = Taccuracy/lendata
-  tpr = Tprecision/lendata
-  tre = Trecall/lendata
-  tfs = TFS/lendata
-  
-  print("accuracy",tac)
-  print("precision",tpr)
-  print("recall",tre)
-  print("FS",tfs)
-  TT = [] ; TT.append(('Total',tac,tpr,tre,tfs))
-  full_result = TT + full_result
-  df = pd.DataFrame(full_result,columns =['Names','accuracy','precision','recall','FS'])
-  df.to_csv('result_'+name+'.csv')
-  return Taccuracy,Tprecision,Trecall,TFS
+  return y_pred,y_true
