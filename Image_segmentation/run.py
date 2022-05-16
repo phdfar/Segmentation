5b674d6d762df3a71d73e32b0dea26506f19daf9
phdfar <103323098+phdfar@users.noreply.github.com>
2022-05-16 13:00:54 +0430

    Update run.py

--- Image_segmentation/run.py
+++ Image_segmentation/run.py
@@ -7,7 +7,7 @@ import accuracy
 from keras.models import load_model
 from keras.callbacks import CSVLogger
 import os
-
+import tensorflow as tf
 global argss
 
 
@@ -39,7 +39,7 @@ def start(args):
     mymodel.summary()
 
     
-    mymodel.compile(optimizer="adam", loss="sparse_categorical_crossentropy")
+    mymodel.compile(optimizer="adam", loss="sparse_categorical_crossentropy",metrics=[tf.keras.metrics.MeanIoU(args.num_class)])
 
     callbacks = [
         keras.callbacks.ModelCheckpoint(args.model_dir, save_best_only=True),CSVLogger(args.model_dir+'_log.csv', append=True, separator=','),CustomCallback()
