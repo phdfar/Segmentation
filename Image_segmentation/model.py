4b7a21d87c466ab212d1ebd14125d9dfd4d0cce4
phdfar <103323098+phdfar@users.noreply.github.com>
2022-05-18 15:07:48 +0430

    Update model.py

--- Image_segmentation/model.py
+++ Image_segmentation/model.py
@@ -7,7 +7,7 @@ def network(args):
   if args.network=='inception_default':
     return inception_default(args.imagesize,args.num_class,args.channel_input)
   if args.network=='inception_deform':
-    return inception_deform(args.imagesize,args.num_class,args.channel_input,args.batchsize)
+    return inception_deform_light(args.imagesize,args.num_class,args.channel_input,args.batchsize)
   if args.network=='inception_mobilenet':
     return inception_mobilenet(args.imagesize,args.num_class)
   if args.network=='mobilenet_s1':
@@ -63,6 +63,74 @@ def inception_default(img_size, num_classes,channel_input):
         previous_block_activation = x  # Set aside next residual
 
     # Add a per-pixel classification layer
+    outputs = layers.Conv2D(num_classes, 3, activation="softmax", padding="same")(x)
+
+    # Define the model
+    model = keras.Model(inputs, outputs)
+    return model
+  
+def inception_deform_light(img_size, num_classes,channel_input):
+    inputs = keras.Input(shape=img_size + (channel_input,))
+
+    ### [First half of the network: downsampling inputs] ###
+
+    # Entry block
+    x = DCL.DeformableConv2D(batch_size = batch_size,
+                                filters = 32,
+                                kernel_size = (3,3),
+                                name = 'layer1',
+                                kernel_initializer='glorot_uniform',strides=2, padding="same")(inputs)
+    x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    # Blocks 1, 2, 3 are identical apart from the feature depth.
+    for filters in [64, 128, 256]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(filters, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    ### [Second half of the network: upsampling inputs] ###
+
+    for filters in [256, 128, 64, 32]:
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.UpSampling2D(2)(x)
+
+        # Project residual
+        residual = layers.UpSampling2D(2)(previous_block_activation)
+        residual = layers.Conv2D(filters, 1, padding="same")(residual)
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    # Add a per-pixel classification layer
+    x = DCL.DeformableConv2D(batch_size = batch_size,
+                            filters = 32,
+                            kernel_size = (3,3),
+                            name = 'layer1',
+                            kernel_initializer='glorot_uniform',strides=2, padding="same")(x)
+      
     outputs = layers.Conv2D(num_classes, 3, activation="softmax", padding="same")(x)
 
     # Define the model
