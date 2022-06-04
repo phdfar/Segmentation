3551a469c7d9542e1737f7523f305b552478b7da
phdfar <103323098+phdfar@users.noreply.github.com>
2022-06-04 19:19:54 +0430

    Update run.py

--- Image_segmentation/models/Spectral/run.py
+++ Image_segmentation/models/Spectral/run.py
@@ -61,4 +61,64 @@ def inception_default(img_size, num_classes,channel_input):
     # Define the model
     model = keras.Model(inputs, outputs)
     return model
-  
+def inception_2i(img_size, num_classes,channel_input):
+    inputs = keras.Input(shape=img_size + (channel_input,))
+    inputs2 = keras.Input(shape=img_size + (1,))
+    z = layers.Activation("relu")(inputs2)
+    ### [First half of the network: downsampling inputs] ###
+
+    # Entry block
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    # Blocks 1, 2, 3 are identical apart from the feature depth.
+    for filters in [64, 128, 256]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(filters, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    ### [Second half of the network: upsampling inputs] ###
+
+    for filters in [256, 128, 64, 32]:
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.UpSampling2D(2)(x)
+
+        # Project residual
+        residual = layers.UpSampling2D(2)(previous_block_activation)
+        residual = layers.Conv2D(filters, 1, padding="same")(residual)
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+        if filters==32:
+          print(x.shape)
+          print(inputs2)
+          z = layers.Multiply()([x, inputs2])
+
+    # Add a per-pixel classification layer
+    outputs = layers.Conv2D(num_classes, 3, activation="softmax", padding="same")(z)
+
+    # Define the model
+    model = keras.Model([inputs,inputs2], outputs)
+    return model
