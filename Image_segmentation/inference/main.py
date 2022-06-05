bb6c404da56b09b3c7ecbba2cca854f8560efddf
phdfar <103323098+phdfar@users.noreply.github.com>
2022-06-05 21:47:49 +0430

    Update main.py

--- Image_segmentation/inference/main.py
+++ Image_segmentation/inference/main.py
@@ -10,7 +10,7 @@ def main(args):
   if (args.classid)[0]==1000:
     args.classid = list(np.linspace(1,40,40).astype('int32'))
     print(args.classid )
-
+  
   run.start(args)
   return args 
 
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     parser.add_argument('--channel_input', type=int ,default=3, required=False)
     parser.add_argument('--mode', type=str ,default='only_vis', required=False)
     parser.add_argument('--task', type=str ,default='semantic', required=False)
-    parser.add_argument('--model_dir', type=str , required=True)
+    parser.add_argument('--model_dir', type=str , required=False)
     parser.add_argument('--basepath', type=str , default='/content/', required=False)
     parser.add_argument('--imagesize', type=tuple_type, required=True)
     parser.add_argument('--colorspace', type=str ,default='rgb', required=False)
