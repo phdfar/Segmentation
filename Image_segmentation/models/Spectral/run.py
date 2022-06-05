e5734557bec1738c0e19d411205f670dc66781c1
phdfar <103323098+phdfar@users.noreply.github.com>
2022-06-05 12:28:51 +0430

    Update run.py

--- Image_segmentation/models/Spectral/run.py
+++ Image_segmentation/models/Spectral/run.py
@@ -3,10 +3,13 @@ from tensorflow import keras
 from keras.models import load_model
 
 def start(args):
-    if args.config!=7:
+    if args.config<7:
         return inception_default(args.imagesize, args.num_class,args.channel_input)
     elif args.config==7:
         return inception_2i(args.imagesize, args.num_class,args.channel_input)
+    elif args.config==8:
+        return inception_2i_extraeig(args.imagesize, args.num_class,args.channel_input)
+    
 def inception_default(img_size, num_classes,channel_input):
     inputs = keras.Input(shape=img_size + (channel_input,))
     ### [First half of the network: downsampling inputs] ###
@@ -68,6 +71,10 @@ def inception_2i(img_size, num_classes,channel_input):
     z = layers.Activation("relu")(inputs2)
     ### [First half of the network: downsampling inputs] ###
 
+    z = layers.Conv2D(1, 3, strides=1, padding="same")(inputs2)
+    z = layers.BatchNormalization()(z)
+    z = layers.Activation("relu")(z)
+    
     # Entry block
     x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
     x = layers.BatchNormalization()(x)
@@ -115,7 +122,73 @@ def inception_2i(img_size, num_classes,channel_input):
         if filters==32:
           print(x.shape)
           print(inputs2)
-          z = layers.Multiply()([x, inputs2])
+          z = layers.Multiply()([x, z])
+
+    # Add a per-pixel classification layer
+    outputs = layers.Conv2D(num_classes, 3, activation="softmax", padding="same")(z)
+
+    # Define the model
+    model = keras.Model([inputs,inputs2], outputs)
+    return model
+
+def inception_2i_extraeig(img_size, num_classes,channel_input):
+    inputs = keras.Input(shape=img_size + (channel_input,))
+    inputs2 = keras.Input(shape=img_size + (2,))
+    z = layers.Activation("relu")(inputs2)
+    ### [First half of the network: downsampling inputs] ###
+
+    z = layers.Conv2D(1, 3, strides=1, padding="same")(inputs2)
+    z = layers.BatchNormalization()(z)
+    z = layers.Activation("relu")(z)
+    
+    # Entry block
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    # Blocks 1, 2, 3 are identical apart from the feature depth.
+    for filters in [64, 128, 256]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(filters, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    ### [Second half of the network: upsampling inputs] ###
+
+    for filters in [256, 128, 64, 32]:
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.UpSampling2D(2)(x)
+
+        # Project residual
+        residual = layers.UpSampling2D(2)(previous_block_activation)
+        residual = layers.Conv2D(filters, 1, padding="same")(residual)
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+        if filters==32:
+          print(x.shape)
+          print(inputs2)
+          z = layers.Multiply()([x, z])
 
     # Add a per-pixel classification layer
     outputs = layers.Conv2D(num_classes, 3, activation="softmax", padding="same")(z)
