373940d3f54b9f586d5019fa4f5b8c0a665d8691
phdfar <103323098+phdfar@users.noreply.github.com>
2022-06-09 19:07:26 +0430

    Update path.py

--- Image_segmentation/Subsidiary/Goodness/path.py
+++ Image_segmentation/Subsidiary/Goodness/path.py
@@ -8,8 +8,8 @@ from tensorflow.keras.preprocessing.image import load_img
 
 jsonf = {'train':'youtube_vis_train.json','valid':'youtube_vis_val.json'}
 def getinfo(args):
-  base_dir=args.basepath+args.data+'/'
-  dataset_json = args.basepath +jsonf[args.data]
+  base_dir=args.jsonpath+args.data+'/'
+  dataset_json = args.jsonpath +jsonf[args.data]
   dataset,meta_info,seqs =  data.parse_generic_video_dataset(base_dir, dataset_json)
   print('Number clip is '+ str(len(seqs)))
   return seqs
