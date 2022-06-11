d85baceacbcd239ddb283554a42ae825a8714fdd
phdfar <103323098+phdfar@users.noreply.github.com>
2022-06-11 20:15:56 +0430

    Update visualize.py

--- Image_segmentation/inference/visualize.py
+++ Image_segmentation/inference/visualize.py
@@ -12,8 +12,12 @@ from keras import backend as K
 import tensorflow as tf
 import csv
 import json
-
-
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn.functional as F
+import denseCRF
 def start(mymodel,seqs,name,args):
     with open(args.basepath +'youtube_vis_val.json', 'r') as fh:
         dataset = json.load(fh)
@@ -29,7 +33,7 @@ def start(mymodel,seqs,name,args):
     run(args,mymodel,seqs,category_label,category_color)
 
 
-def draw_semantic(mask,img,cat_mask,size,category_color,category_label,args,imagepath):
+def draw_semantic(args,outputs,mask,img,cat_mask,size,category_color,category_label,args,imagepath):
     font = cv2.FONT_HERSHEY_SIMPLEX;
     footer1 = np.zeros((40,args.imagesize[1],3),'uint8')+200;al=2;
     rgb = img.copy()
@@ -47,9 +51,16 @@ def draw_semantic(mask,img,cat_mask,size,category_color,category_label,args,imag
     filename = imagepath.split('/'); filename=filename[-2]+'_'+filename[-1]
     res.save('result/'+filename)
         
-def draw_binary(mask,img,cat_mask,size,category_color,category_label,args,imagepath):
+def draw_binary(args,outputs,mask,img,cat_mask,size,category_color,category_label,args,imagepath):
     #font = cv2.FONT_HERSHEY_SIMPLEX;
     #footer1 = np.zeros((40,args.imagesize[1],3),'uint8')+200;al=2;
+    w1    = 10.0  # weight of bilateral term
+    alpha = 80    # spatial std
+    beta  = 13    # rgb  std
+    w2    = 3.0   # weight of spatial term
+    gamma = 3     # spatial std
+    it    = 5.0   # iteration
+    param = (w1, alpha, beta, w2, gamma, it)
     rgb = img.copy()
     for lb in cat_mask:
         if lb!=0:
@@ -59,6 +70,16 @@ def draw_binary(mask,img,cat_mask,size,category_color,category_label,args,imagep
             #text = category_label[lb] + ' ' + str((len(tp[0])*100)/size)[:4]+'%'
             #cv2.putText(footer1, text, (al,footer1.shape[0]-20), font, 0.4, color, 1, cv2.LINE_AA);al+=160;
             rgb[tp]=(color+ rgb[tp])//2
+    if args.corrector=='crf':
+        rgb_c = img.copy()
+        #unary_potentials = F.one_hot(torch.from_numpy(bestcluster).long(), num_classes=2)
+        segmap_crf = denseCRF.densecrf(rgb, outputs, param)  # (H_pad, W_pad)
+        for lb in [0,1]:
+            if lb!=0:
+                color = (0,255,0)
+                tp = np.where(segmap_crf==lb);                
+                rgb_c[tp]=(color+ rgb_c[tp])//2
+                rgb = np.concatenate((rgb,rgb_c),axis=1);
             
     #result = np.concatenate((rgb,footer1),axis=0);
     res = keras.preprocessing.image.array_to_img(rgb)
@@ -108,6 +129,6 @@ def run(args,mymodel,seqs,category_label,category_color):
                 mask = np.expand_dims(mask, axis=-1)
                 mask = mask[:,:,0];
             cat_mask = list(set(mask.ravel().tolist()))
-            dispatcher_draw[args.task](mask,inputs[p],cat_mask,size,category_color,category_label,args,imagepath[p])
+            dispatcher_draw[args.task](args,outputs[p],mask,inputs[p],cat_mask,size,category_color,category_label,args,imagepath[p])
     
       
