a5eb538709b49c0fa9823f7d8506255818c1627e
phdfar <iplserver01@gmail.com>
2022-06-17 20:04:05 +0430

    fs

--- Image_segmentation/Subsidiary/Goodness/main.py
+++ Image_segmentation/Subsidiary/Goodness/main.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
     parser.add_argument('--mode', type=str ,default='only_vis', required=False)
     parser.add_argument('--task', type=str ,default='semantic', required=False)
     parser.add_argument('--data', type=str ,default='train', required=False)
+    parser.add_argument('--network', type=str ,default='inception_default', required=False)
     parser.add_argument('--score', type=str ,default='optical', required=False)
     parser.add_argument('--score_path', type=str ,default='train_optical', required=False)
     parser.add_argument('--jsonpath', type=str ,default='/content/', required=False)
