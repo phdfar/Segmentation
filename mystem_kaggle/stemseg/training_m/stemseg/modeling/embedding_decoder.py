3c1c3cecbf2c5e6f0255277d59d432635c382592
phdfar <iplserver01@gmail.com>
2022-07-20 12:10:56 +0430

    fff

--- mystem_kaggle/stemseg/training_m/stemseg/modeling/embedding_decoder.py
+++ mystem_kaggle/stemseg/training_m/stemseg/modeling/embedding_decoder.py
@@ -4,9 +4,55 @@ from stemseg.utils.global_registry import GlobalRegistry
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 EMBEDDING_HEAD_REGISTRY = GlobalRegistry.get("EmbeddingHead")
 
+def logsumexp_2d(tensor):
+    tensor_flatten = tensor.view(tensor.size(0), tensor.size(1), -1)
+    s, _ = torch.max(tensor_flatten, dim=2, keepdim=True)
+    outputs = s + (tensor_flatten - s).exp().sum(dim=2, keepdim=True).log()
+    return outputs
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+class ChannelGate(nn.Module):
+    def __init__(self, gate_channels, reduction_ratio=16, pool_types=['avg', 'max']):
+        super(ChannelGate, self).__init__()
+        self.gate_channels = gate_channels
+        self.mlp = nn.Sequential(
+            Flatten(),
+            nn.Linear(gate_channels, gate_channels // reduction_ratio),
+            nn.ReLU(),
+            nn.Linear(gate_channels // reduction_ratio, gate_channels)
+            )
+        self.pool_types = pool_types
+    def forward(self, x):
+        channel_att_sum = None
+        for pool_type in self.pool_types:
+            if pool_type=='avg':
+                avg_pool = F.avg_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp( avg_pool )
+            elif pool_type=='max':
+                max_pool = F.max_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp( max_pool )
+            elif pool_type=='lp':
+                lp_pool = F.lp_pool2d( x, 2, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp( lp_pool )
+            elif pool_type=='lse':
+                # LSE pool only
+                lse_pool = logsumexp_2d(x)
+                channel_att_raw = self.mlp( lse_pool )
+
+            if channel_att_sum is None:
+                channel_att_sum = channel_att_raw
+            else:
+                channel_att_sum = channel_att_sum + channel_att_raw
+
+        scale = F.sigmoid( channel_att_sum ).unsqueeze(2).unsqueeze(3).expand_as(x)
+        return x * scale
 
 @EMBEDDING_HEAD_REGISTRY.add("squeeze_expand_decoder")
 class SqueezingExpandDecoder(nn.Module):
@@ -16,6 +62,17 @@ class SqueezingExpandDecoder(nn.Module):
         super().__init__()
 
         PoolingLayerCallbacks = get_pooling_layer_creator(PoolType)
+        gate_channels = 256
+        reduction_ratio = 16
+
+        self.pool_types = ['avg','max']
+
+        self.mlp = nn.Sequential(
+            Flatten(),
+            nn.Linear(gate_channels, gate_channels // reduction_ratio),
+            nn.ReLU(),
+            nn.Linear(gate_channels // reduction_ratio, gate_channels)
+            )
 
         self.block_32x = nn.Sequential(
             ConvType(in_channels, inter_channels[0], 3, stride=1, padding=1, dilation=1),
@@ -79,6 +136,8 @@ class SqueezingExpandDecoder(nn.Module):
         )
         self.conv_4 = nn.Conv3d(inter_channels[2] + inter_channels[3], inter_channels[3], 1, bias=False)
 
+        #self.conv_44 = nn.Conv3d(256, 128, 1, bias=False)
+
         self.embedding_size = embedding_size
 
         n_free_dims = get_nb_free_dims(experimental_dims)
@@ -99,6 +158,33 @@ class SqueezingExpandDecoder(nn.Module):
         self.register_buffer("time_scale", torch.tensor(1.0, dtype=torch.float32))
 
     def forward(self, x):
+
+        def CAM(x):
+          channel_att_sum = None
+          for pool_type in self.pool_types:
+              if pool_type=='avg':
+                  avg_pool = F.avg_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                  channel_att_raw = self.mlp( avg_pool )
+              elif pool_type=='max':
+                  max_pool = F.max_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                  channel_att_raw = self.mlp( max_pool )
+              elif pool_type=='lp':
+                  lp_pool = F.lp_pool2d( x, 2, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                  channel_att_raw = self.mlp( lp_pool )
+              elif pool_type=='lse':
+                  lse_pool = logsumexp_2d(x)
+                  channel_att_raw = self.mlp( lse_pool )
+
+              if channel_att_sum is None:
+                  channel_att_sum = channel_att_raw
+              else:
+                  channel_att_sum = channel_att_sum + channel_att_raw
+
+          scale = F.sigmoid( channel_att_sum ).unsqueeze(2).unsqueeze(3).expand_as(x)
+          x = x * scale
+          return x
+
+
         """
         :param x: list of multiscale feature map tensors of shape [N, C, T, H, W]. For this implementation, there
         should be 4 features maps in increasing order of spatial dimensions
@@ -107,6 +193,44 @@ class SqueezingExpandDecoder(nn.Module):
         assert len(x) == 4, "Expected 4 feature maps, got {}".format(len(x))
 
         feat_map_32x, feat_map_16x, feat_map_8x, feat_map_4x = x
+
+
+        for i in range(0,8):
+          y = feat_map_4x[:,:,i,:,:]
+          y = torch.unsqueeze(CAM(y),2)
+          if i==0:
+            z = y;
+          else:
+             z = torch.cat((z, y),2)
+        feat_map_4x = z
+
+        for i in range(0,8):
+          y = feat_map_8x[:,:,i,:,:]
+          y = torch.unsqueeze(CAM(y),2)
+          if i==0:
+            z = y;
+          else:
+             z = torch.cat((z, y),2)
+        feat_map_8x = z
+
+        for i in range(0,8):
+          y = feat_map_16x[:,:,i,:,:]
+          y = torch.unsqueeze(CAM(y),2)
+          if i==0:
+            z = y;
+          else:
+             z = torch.cat((z, y),2)
+        feat_map_16x = z
+
+        for i in range(0,8):
+          y = feat_map_32x[:,:,i,:,:]
+          y = torch.unsqueeze(CAM(y),2)
+          if i==0:
+            z = y;
+          else:
+             z = torch.cat((z, y),2)
+        feat_map_32x = z
+
 
         feat_map_32x = self.block_32x(feat_map_32x)
 
@@ -128,6 +252,20 @@ class SqueezingExpandDecoder(nn.Module):
         x = torch.cat((x, feat_map_4x), 1)
         x = self.conv_4(x)
 
+
+        #x=self.conv_44(feat_map_4x)
+
+        """
+        for i in range(0,8):
+          y = x[:,:,i,:,:]
+          y = torch.unsqueeze(CAM(y),2)
+          if i==0:
+            z = y;
+          else:
+             z = torch.cat((z, y),2)
+        x = z
+        """
+
         embeddings = self.conv_embedding(x)
         if self.tanh_activation:
             embeddings = (embeddings * 0.25).tanh()
@@ -141,7 +279,6 @@ class SqueezingExpandDecoder(nn.Module):
             output = torch.cat((embeddings, variances, seediness), dim=1)
         else:
             output = torch.cat((embeddings, variances), dim=1)
-
         return output
 
 
