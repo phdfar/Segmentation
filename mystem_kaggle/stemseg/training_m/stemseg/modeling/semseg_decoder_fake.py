6cbe6e3abc872e99cafed8ffd036286ed5e079c4
phdfar <iplserver01@gmail.com>
2022-08-13 20:18:07 +0430

    fff

--- mystem_kaggle/stemseg/training_m/stemseg/modeling/semseg_decoder_fake.py
+++ mystem_kaggle/stemseg/training_m/stemseg/modeling/semseg_decoder_fake.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from stemseg.modeling.common import UpsampleTrilinear3D, AtrousPyramid3D, get_pooling_layer_creator, \
     get_temporal_scales
@@ -8,6 +9,17 @@ from stemseg.utils.global_registry import GlobalRegistry
 
 SEMSEG_HEAD_REGISTRY = GlobalRegistry.get("SemsegHead")
 
+
+def logsumexp_2d(tensor):
+    tensor_flatten = tensor.view(tensor.size(0), tensor.size(1), -1)
+    s, _ = torch.max(tensor_flatten, dim=2, keepdim=True)
+    outputs = s + (tensor_flatten - s).exp().sum(dim=2, keepdim=True).log()
+    return outputs
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+    
 
 @SEMSEG_HEAD_REGISTRY.add("squeeze_expand_decoder")
 class SqueezeExpandDecoder(nn.Module):
@@ -19,6 +31,19 @@ class SqueezeExpandDecoder(nn.Module):
         assert tuple(feature_scales) == (4, 8, 16, 32)
 
         PoolingLayerCallbacks = get_pooling_layer_creator(PoolType)
+        
+        gate_channels = 256
+        reduction_ratio = 16
+
+        self.pool_types = ['avg','max']
+
+        self.mlp = nn.Sequential(
+            Flatten(),
+            nn.Linear(gate_channels, gate_channels // reduction_ratio),
+            nn.ReLU(),
+            nn.Linear(gate_channels // reduction_ratio, gate_channels)
+            )
+
 
         self.block_32x = nn.Sequential(
             ConvType(in_channels, inter_channels[0], 3, stride=1, padding=1),
@@ -89,9 +114,73 @@ class SqueezeExpandDecoder(nn.Module):
         self.has_foreground_channel = foreground_channel
 
     def forward(self, x):
+        
+        def CAM(x):
+          channel_att_sum = None
+          for pool_type in self.pool_types:
+              if pool_type=='avg':
+                  avg_pool = F.avg_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                  channel_att_raw = self.mlp( avg_pool )
+              elif pool_type=='max':
+                  max_pool = F.max_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                  channel_att_raw = self.mlp( max_pool )
+              elif pool_type=='lp':
+                  lp_pool = F.lp_pool2d( x, 2, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                  channel_att_raw = self.mlp( lp_pool )
+              elif pool_type=='lse':
+                  lse_pool = logsumexp_2d(x)
+                  channel_att_raw = self.mlp( lse_pool )
+
+              if channel_att_sum is None:
+                  channel_att_sum = channel_att_raw
+              else:
+                  channel_att_sum = channel_att_sum + channel_att_raw
+
+          scale = F.sigmoid( channel_att_sum ).unsqueeze(2).unsqueeze(3).expand_as(x)
+          x = x * scale
+          return x
+      
         assert len(x) == 4, "Expected 4 feature maps, got {}".format(len(x))
 
         feat_map_32x, feat_map_16x, feat_map_8x, feat_map_4x = x[::-1]
+        
+        
+        for i in range(0,8):
+          y = feat_map_4x[:,:,i,:,:]
+          y = torch.unsqueeze(CAM(y),2)
+          if i==0:
+            z = y;
+          else:
+             z = torch.cat((z, y),2)
+        feat_map_4x = z
+
+        for i in range(0,8):
+          y = feat_map_8x[:,:,i,:,:]
+          y = torch.unsqueeze(CAM(y),2)
+          if i==0:
+            z = y;
+          else:
+             z = torch.cat((z, y),2)
+        feat_map_8x = z
+
+        for i in range(0,8):
+          y = feat_map_16x[:,:,i,:,:]
+          y = torch.unsqueeze(CAM(y),2)
+          if i==0:
+            z = y;
+          else:
+             z = torch.cat((z, y),2)
+        feat_map_16x = z
+
+        for i in range(0,8):
+          y = feat_map_32x[:,:,i,:,:]
+          y = torch.unsqueeze(CAM(y),2)
+          if i==0:
+            z = y;
+          else:
+             z = torch.cat((z, y),2)
+        feat_map_32x = z
+        
 
         feat_map_32x = self.block_32x(feat_map_32x)
 
