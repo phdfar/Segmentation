1050b914a2d8cb1ecc82021f396f56895c634eef
phdfar <iplserver01@gmail.com>
2022-08-23 19:39:42 +0430

    dddd

--- mystem_kaggle/stemseg/training_m/stemseg/modeling/tcbam_old.py
+++ mystem_kaggle/stemseg/training_m/stemseg/modeling/tcbam_old.py
@@ -7,8 +7,8 @@ class BasicConv(nn.Module):
     def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, groups=1, relu=True, bn=True, bias=False):
         super(BasicConv, self).__init__()
         self.out_channels = out_planes
-        self.conv = nn.Conv3d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
-        self.bn = nn.BatchNorm3d(out_planes,eps=1e-5, momentum=0.01, affine=True) if bn else None
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
+        self.bn = nn.BatchNorm2d(out_planes,eps=1e-5, momentum=0.01, affine=True) if bn else None
         self.relu = nn.ReLU() if relu else None
 
     def forward(self, x):
@@ -69,34 +69,16 @@ def logsumexp_2d(tensor):
     outputs = s + (tensor_flatten - s).exp().sum(dim=2, keepdim=True).log()
     return outputs
 
-class ChannelPool(nn.Module):
-    def forward(self, x):
-        return torch.cat( (torch.max(x,1)[0].unsqueeze(1), torch.mean(x,1).unsqueeze(1)), dim=1 )
-
-class SpatialGate(nn.Module):
-    def __init__(self):
-        super(SpatialGate, self).__init__()
-        kernel_size = 7
-        self.compress = ChannelPool()
-        self.spatial = BasicConv(2, 1, kernel_size, stride=1, padding=(kernel_size-1) // 2, relu=False)
-    def forward(self, x):
-        x_compress = self.compress(x)
-        x_out = self.spatial(x_compress)
-        scale = F.sigmoid(x_out) # broadcasting
-        return x * scale
-    
 class TCBAM(nn.Module):
     def __init__(self, gate_channels,gate_temporal, reduction_ratio=16, pool_types=['avg', 'max'], no_spatial=False):
         super(TCBAM, self).__init__()
         self.ChannelGate1 = ChannelGate(gate_channels, reduction_ratio, pool_types)
         self.ChannelGate2 = ChannelGate(gate_temporal, 2, pool_types)
-        self.SpatialGate = SpatialGate()
 
     def forward(self, x):
         x_out1 = self.ChannelGate1(x)
         x_transpose = torch.permute(x_out1, (0, 2, 1, 3, 4))
         x_out2 = self.ChannelGate2(x_transpose)
         x_transpose = torch.permute(x_out2, (0, 2, 1, 3, 4))
-        x_sp = self.SpatialGate(x_transpose)
-        x_out = x + x_sp
+        x_out = x + x_transpose
         return x_out
