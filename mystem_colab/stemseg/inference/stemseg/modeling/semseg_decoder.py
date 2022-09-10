2acb65ef813fbca08689151e9a62179a897a9fed
phdfar <iplserver01@gmail.com>
2022-09-10 21:01:27 +0430

    ddd

--- mystem_colab/stemseg/inference/stemseg/modeling/semseg_decoder.py
+++ mystem_colab/stemseg/inference/stemseg/modeling/semseg_decoder.py
@@ -98,9 +98,10 @@ class SqueezeExpandDecoder(nn.Module):
         assert len(x) == 4, "Expected 4 feature maps, got {}".format(len(x))
 
         feat_map_32x, feat_map_16x, feat_map_8x, feat_map_4x = x[::-1];#print('feat_map_32xxxxxxxxxxxxxxxx',feat_map_32x.shape)        
-        #F4
-        feat_map_4x = self.tcbam(feat_map_4x)
-        MC_F4 = self.mc(feat_map_4x)
+        
+        #F32
+        feat_map_32x = self.tcbam(feat_map_32x)
+        MC_F32 = self.mc(feat_map_32x)
         
         
         def todo(z,MCIN):
@@ -110,9 +111,10 @@ class SqueezeExpandDecoder(nn.Module):
             y = torch.permute(t, (0, 2, 1, 3, 4))
             return y
             
-        feat_map_8x = todo(feat_map_8x,MC_F4)
-        feat_map_16x = todo(feat_map_16x,MC_F4)
-        feat_map_32x = todo(feat_map_32x,MC_F4)
+        feat_map_4x = todo(feat_map_4x,MC_F32)
+        feat_map_8x = todo(feat_map_8x,MC_F32)
+        feat_map_16x = todo(feat_map_16x,MC_F32)
+
         
         
         feat_map_32x = self.block_32x(feat_map_32x)
