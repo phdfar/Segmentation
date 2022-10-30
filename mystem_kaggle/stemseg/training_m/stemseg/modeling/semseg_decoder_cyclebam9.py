35dc915d6a49d8ce60d02166dee8dcc57e1086ee
phdfar <103323098+phdfar@users.noreply.github.com>
2022-10-30 22:02:42 +0330

    Update semseg_decoder_cyclebam9.py

--- mystem_kaggle/stemseg/training_m/stemseg/modeling/semseg_decoder_cyclebam9.py
+++ mystem_kaggle/stemseg/training_m/stemseg/modeling/semseg_decoder_cyclebam9.py
@@ -132,7 +132,7 @@ class SqueezeExpandDecoder(nn.Module):
         x = self.conv_4(x)
 
         #temporal attention
-        for i in range(0,8,2):
+        for i in range(0,8):
           fs = x[:,:,i,:,:]  #[1 C H W]
           fs = self.maxpool(fs)
           if i==0:
