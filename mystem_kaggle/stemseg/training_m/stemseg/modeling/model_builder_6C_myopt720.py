cd81102635c8e5bc530cd3d1b1ba303aee739fcd
phdfar <iplserver01@gmail.com>
2022-11-29 12:29:54 +0330

    fff

--- mystem_kaggle/stemseg/training_m/stemseg/modeling/model_builder_6C_myopt720.py
+++ mystem_kaggle/stemseg/training_m/stemseg/modeling/model_builder_6C_myopt720.py
@@ -200,8 +200,7 @@ class TrainingModel(nn.Module):
         images_tensorz = torch.permute(images_tensorz,(0,3,1,2)).float()
         #print('images_tensorz',images_tensorz.size())
         #print('images_tensor',images_tensor.size())
-        self.backbone2 = self.backbone.clone()
-        self.backbone2 = self.backbone2.to(device='cuda:1')
+        
         
         restore_dict = torch.load('/kaggle/working/pretrained/mask_rcnn_R_101_FPN_backbone.pth',map_location="cuda:1")
         backbone_type = cfg.MODEL.BACKBONE.TYPE
