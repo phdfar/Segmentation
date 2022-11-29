8528e55c613e2226fbba9c09b8cb9fa8fca94e19
phdfar <iplserver01@gmail.com>
2022-11-29 16:40:02 +0330

    fff

--- SELECT.py
+++ SELECT.py
@@ -5,16 +5,16 @@ from argparse import ArgumentParser
 def init(args):
   if args.mode=='raftrgb':
       
-    os.system('!cp Segmentation/mystem_colab/stemseg/inference/stemseg/data/generic_video_dataset_parser_6C.py  Segmentation/mystem_colab/stemseg/inference/stemseg/data/generic_video_dataset_parser.py')
-    os.system('!cp Segmentation/mystem_kaggle/stemseg/training_m/stemseg/data/common_6C.py Segmentation/mystem_colab/stemseg/inference/stemseg/data/common.py')
-    os.system('!cp Segmentation/mystem_kaggle/stemseg/training_m/stemseg/structures/image_list_6C.py Segmentation/mystem_colab/stemseg/inference/stemseg/structures/image_list.py')
-    os.system('!cp /content/Segmentation/mystem_colab/stemseg/inference/stemseg/modeling/model_builder_color.py Segmentation/mystem_colab/stemseg/inference/stemseg/modeling/model_builder.py')
-    os.system('!cp /content/Segmentation/mystem_colab/stemseg/inference/stemseg/modeling/inference_model_6C.py /content/Segmentation/mystem_colab/stemseg/inference/stemseg/modeling/inference_model.py')
+    os.system('cp Segmentation/mystem_colab/stemseg/inference/stemseg/data/generic_video_dataset_parser_6C.py  Segmentation/mystem_colab/stemseg/inference/stemseg/data/generic_video_dataset_parser.py')
+    os.system('cp Segmentation/mystem_kaggle/stemseg/training_m/stemseg/data/common_6C.py Segmentation/mystem_colab/stemseg/inference/stemseg/data/common.py')
+    os.system('cp Segmentation/mystem_kaggle/stemseg/training_m/stemseg/structures/image_list_6C.py Segmentation/mystem_colab/stemseg/inference/stemseg/structures/image_list.py')
+    os.system('cp /content/Segmentation/mystem_colab/stemseg/inference/stemseg/modeling/model_builder_color.py Segmentation/mystem_colab/stemseg/inference/stemseg/modeling/model_builder.py')
+    os.system('cp /content/Segmentation/mystem_colab/stemseg/inference/stemseg/modeling/inference_model_6C.py /content/Segmentation/mystem_colab/stemseg/inference/stemseg/modeling/inference_model.py')
     
 
 
   if args.mode=='emcy2':
-    os.system('!cp Segmentation/mystem_kaggle/stemseg/training_m/stemseg/modeling/embedding_decoder_cyclebam2.py Segmentation/mystem_colab/stemseg/inference/stemseg/modeling/embedding_decoder.py')
+    os.system('cp Segmentation/mystem_kaggle/stemseg/training_m/stemseg/modeling/embedding_decoder_cyclebam2.py Segmentation/mystem_colab/stemseg/inference/stemseg/modeling/embedding_decoder.py')
 
  
 if __name__ == '__main__':
