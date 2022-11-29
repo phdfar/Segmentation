161a0295f725687d69a6a735b4c375c9cd889bf2
phdfar <iplserver01@gmail.com>
2022-11-29 12:35:39 +0330

    fff

--- mystem_kaggle/stemseg/training_m/stemseg/modeling/model_builder_6C_myopt720.py
+++ mystem_kaggle/stemseg/training_m/stemseg/modeling/model_builder_6C_myopt720.py
@@ -217,9 +217,9 @@ class TrainingModel(nn.Module):
 
         if cfg.TRAINING.FREEZE_BACKBONE:
             with torch.no_grad():
-                features2 = backbone2(images_tensorz)
+                features2 = backbone2(images_tensorz.cuda())
         else:
-            features2 = backbone2(images_tensorz)
+            features2 = backbone2(images_tensorz.cuda())
 
 
 
