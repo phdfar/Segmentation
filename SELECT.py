a48a45eac1891bc7d2bfc2348822bf40767ccf28
phdfar <iplserver01@gmail.com>
2022-11-29 17:34:15 +0330

    fff

--- SELECT.py
+++ SELECT.py
@@ -15,8 +15,11 @@ def init(args):
 
   if args.mode=='emcy2':
     os.system('cp Segmentation/mystem_kaggle/stemseg/training_m/stemseg/modeling/embedding_decoder_cyclebam2.py Segmentation/mystem_colab/stemseg/inference/stemseg/modeling/embedding_decoder.py')
-
- 
+    
+  if args.mode=='emcy4':
+    os.system('cp Segmentation/mystem_kaggle/stemseg/training_m/stemseg/modeling/embedding_decoder_cyclebam4.py Segmentation/mystem_colab/stemseg/inference/stemseg/modeling/embedding_decoder.py')
+    
+     
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--mode', type=str ,default='main', required=False)
