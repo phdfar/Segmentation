260dd07cc4e9091332337b493ec6e3ff32bfe394
phdfar <103323098+phdfar@users.noreply.github.com>
2024-04-20 06:05:13 -0700

    Update update.py

--- update.py
+++ update.py
@@ -144,6 +144,31 @@ class LookupScaler(nn.Module):
         self.output_dim = output_dim
         self.max_multiplier = max_multiplier
         self.max_translation = max_translation
+        self.convert_conv = nn.Conv2d(2 * input_dim, 2 * input_dim, 1
+
+        self.model_scale = nn.Sequential(nn.Linear(4 * input_dim, (output_dim // 2) * output_size),nn.Dropout(p=0.2),
+                                   nn.Sigmoid())
+        self.model_add = nn.Sequential(nn.Linear(4 * input_dim, (output_dim // 2) * output_size),nn.Dropout(p=0.2),
+                                   nn.Sigmoid())
+    
+    def forward(self, context_map, hidden_state):
+        assert(context_map.shape[1] == self.input_dim)
+        assert(hidden_state.shape[1] == self.input_dim)
+        convert_map = self.convert_conv(torch.cat([context_map, hidden_state], dim=1).type(torch.float32))
+        lookup_context = torch.cat([torch.amax(convert_map, dim=(2, 3)),
+                                    torch.amin(convert_map, dim=(2, 3))], dim=-1).type(torch.float32)
+        scale = self.model_scale(lookup_context).view(-1, self.output_size, self.output_dim // 2) * self.max_multiplier + 1
+        add = self.model_add(lookup_context).view(-1, self.output_size, self.output_dim // 2) * self.max_translation
+        return torch.cat([scale, add], dim=-1)
+        
+class LookupScalerx(nn.Module):
+    def __init__(self, input_dim=128, output_size=4, output_dim=4, max_multiplier=2, max_translation=2):
+        super(LookupScaler, self).__init__()
+        self.input_dim = input_dim
+        self.output_size = output_size
+        self.output_dim = output_dim
+        self.max_multiplier = max_multiplier
+        self.max_translation = max_translation
         self.convert_conv = nn.Conv2d(2 * input_dim, 2 * input_dim, 1)
         self.model_scale = nn.Sequential(nn.Linear(4 * input_dim, (output_dim // 2) * output_size),
                                    nn.Sigmoid())
